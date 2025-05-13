b865e21357bb0a6e9a369e378e3eb6b07dc4d4bb
GoNEAR <cagatay.morgul@outlook.com>
2025-05-13 12:18:55 +0200

    li change couleur quand on appui dessu

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,7 +1,27 @@
 import "./User.css";
 import userImg from "../../assets/images/image-jeremy.png";
+import { useState } from "react";
 
 function User() {
+  const [daily, setDaily] = useState(true);
+  const [weekly, setWeekly] = useState(false);
+  const [monthly, setMonthly] = useState(false);
+
+  function handleClick(e) {
+    if (e.target.textContent == "Daily") {
+      setDaily(true);
+      setWeekly(false);
+      setMonthly(false);
+    } else if (e.target.textContent == "Weekly") {
+      setDaily(false);
+      setWeekly(true);
+      setMonthly(false);
+    } else {
+      setDaily(false);
+      setWeekly(false);
+      setMonthly(true);
+    }
+  }
   return (
     <>
       <div className="user">
@@ -16,9 +36,15 @@ function User() {
         </div>
         <div className="userFooter">
           <ul>
-            <li>Daily</li>
-            <li>Weekly</li>
-            <li>Monthly</li>
+            <li onClick={(e) => handleClick(e)} style={daily ? { color: "white" } : {}}>
+              Daily
+            </li>
+            <li onClick={(e) => handleClick(e)} style={weekly ? { color: "white" } : {}}>
+              Weekly
+            </li>
+            <li onClick={(e) => handleClick(e)} style={monthly ? { color: "white" } : {}}>
+              Monthly
+            </li>
           </ul>
         </div>
       </div>
