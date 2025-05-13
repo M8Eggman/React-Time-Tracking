47fe9af0faeb1b0e1fd05951effda0f5d0d7f6fa
GoNEAR <cagatay.morgul@outlook.com>
2025-05-13 10:03:04 +0200

    card component structure

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,25 @@
 import "./Card.css";
 
 function Card() {
-  return <></>;
+  return (
+    <>
+      <div className="card">
+        <div className="carHeader">
+          <img src="" alt="" />
+        </div>
+        <div className="cardBody">
+          <div className="cardBodyTitle">
+            <p></p>
+            <img src="" alt="" />
+          </div>
+          <div className="cardBodyHours">
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
